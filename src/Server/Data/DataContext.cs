6a585be2d3ee327d1ecd00ba8361d160dfb71366
Yevhenii Nabok <1337niceshot@gmail.com>
2022-08-10 15:50:59 +0300

    Create user model and database

---
+++ src/Server/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿namespace homework_prac4.Server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<UserModel> Users { get; set; } 
+
+        public DataContext(DbContextOptions<DataContext> options)
+            :base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserModel>().HasData(
+                new UserModel { Id = 1, FirstName = "Anna", LastName = "Walker", Age = 21, Gender = "Female" },
+                new UserModel { Id = 2, FirstName = "Bob", LastName = "Marley", Age = 47, Gender = "Male" }
+                );
+
+        }
+    }
+}
